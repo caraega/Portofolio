abb2a9e51799e9985a672f1d98f505686d35a0f9
caraega <egachatgpt@gmail.com>
2024-07-18 21:18:30 +0700

    add contact

--- main.js
+++ main.js
@@ -31,3 +31,25 @@ function toggleDetail (e) {
    }
    
    
+   function onFormSubmit(e){
+      e.preventDefault()
+      const email = $("#inp_email")
+      const subject = $("#inp_subject")
+      const message = $("#inp_message")
+      
+
+      if(!$(email).val()){
+         alert("Email Is Required")
+      }else if (!$(subject).val()) {
+         alert("Subject Is Required")
+      }else if (!$(message).val()){
+         alert("Message is required")
+
+         }else {
+            $(email).val("")
+            $(subject).val("")
+            $(message).val("")
+            alert("Form Submitted")
+      }
+         //    console.log ($(subject).val())
+   }
